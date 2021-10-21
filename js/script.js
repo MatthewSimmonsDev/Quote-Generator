6a4772f7ea1298a09473bbc60002ee533eccf4fd
Matthew Simmons <matthewsimmonsdev@gmail.com>
2021-10-20 23:50:46 -0400

    Barebones Functionality

--- js/script.js
+++ js/script.js
@@ -7,22 +7,35 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
+let quotation = '';
+
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {quote: "Hello there."},
+  {quote: "Hi There"}
+]
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote(){
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  quotation = quotes[randomNumber].quote;
+  return quotation;
+}
 
 
 /***
  * `printQuote` function
 ***/
-
+function printQuote(){
+  let randomQuote = getRandomQuote();
+  let htmlString = `<p class="quote">${randomQuote}</p>`;
+  document.getElementById('quote-box').innerHTML = htmlString;
+}
 
 
 /***
